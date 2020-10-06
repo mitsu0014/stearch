a25e36f6eb9cb2014e3cbf5bbd895e323fd4fbdf
mitsu0014 <70260498+mitsu0014@users.noreply.github.com>
2020-10-06 17:22:13 +0900

    フラッシュメッセージ

---
+++ app/javascript/flash.js
@@ -0,0 +1,5 @@
+$(function(){
+  setTimeout(function(){
+    $ ('.flash').slideUp();
+  }, 2000);
+  });
--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2020_10_05_092437) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "provider"
     t.string "uid"
-    t.string "name", null: false
+    t.string "name"
     t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

3d07ad02bacf5561ee9da250c614e53c3608bfc6
mitsu0014 <70260498+mitsu0014@users.noreply.github.com>
2020-10-11 15:56:13 +0900

    ユーザー登録結合テストコード

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+def basic_pass(path)
+  username = ENV['BASIC_AUTH_USER']
+  password = ENV['BASIC_AUTH_PASSWORD']
+  visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
+end
+
+RSpec.describe "Users", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  context 'ユーザー新規登録ができるとき' do 
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      basic_pass root_path
+      expect(page).to have_content('新規登録')
+      visit new_user_registration_path
+      fill_in 'ユーザーネーム', with: @user.nickname
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      fill_in 'パスワード(確認)', with: @user.password_confirmation
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
+      expect(current_path).to eq root_path
+      expect(page).to have_content('ログアウト')
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+        end
+      end
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      basic_pass root_path
+      expect(page).to have_content('新規登録')
+      visit new_user_registration_path
+      fill_in 'ユーザーネーム', with: ""
+      fill_in 'メールアドレス', with: ""
+      fill_in 'パスワード', with: ""
+      fill_in 'パスワード(確認)', with: ""
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
+      expect(current_path).to eq "/users"
+    end
+  end
+end

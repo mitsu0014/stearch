4a1e4010c10fdd72f6edc3d98066bf702677d1ea
mitsu0014 <70260498+mitsu0014@users.noreply.github.com>
2020-10-18 12:28:26 +0900

    登録フォーム

---
+++ db/migrate/20201018025927_add_columns_to_performances.rb
@@ -0,0 +1,7 @@
+class AddColumnsToPerformances < ActiveRecord::Migration[6.0]
+  def change
+    add_column :performances, :address, :string
+    add_column :performances, :latitude, :float
+    add_column :performances, :longitude, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_052452) do
+ActiveRecord::Schema.define(version: 2020_10_18_025927) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(version: 2020_10_07_052452) do
     t.string "theatre"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

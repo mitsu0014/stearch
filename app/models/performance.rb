4ecbfbb9fb84d2e8496041ca188fa4178175004e
mitsu0014 <70260498+mitsu0014@users.noreply.github.com>
2020-10-19 11:38:08 +0900

    訂正

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -6,7 +6,7 @@ class Performance < ApplicationRecord
   belongs_to_active_hash :area
 
   geocoded_by :address
-  after_validation :geocode
+  after_validation :geocode, :if => :address_changed?
 
   with_options presence: true do
     validates  :title

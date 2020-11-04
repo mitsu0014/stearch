7bc5b6c93d0328b6ba687f827aadd6037e4438ff
mitsu0014 <70260498+mitsu0014@users.noreply.github.com>
2020-11-04 14:27:54 +0900

    訂正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,7 @@ Rails.application.configure do
   password:             ENV['GMAIL_PASS'],
   authentication:       'plain',
   enable_starttls_auto:  true
+}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

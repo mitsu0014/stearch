64aa864b514ecfd1adcf9a4784a40cc057075aa6
mitsu0014 <70260498+mitsu0014@users.noreply.github.com>
2020-11-04 13:36:50 +0900

    本番環境設定

--- db/migrate/20201102091223_create_inquiries.rb
+++ db/migrate/20201102091223_create_inquiries.rb
@@ -2,7 +2,7 @@ class CreateInquiries < ActiveRecord::Migration[6.0]
   def change
     create_table :inquiries do |t|
       t.string :name
-      t.string :message
+      t.string :message, null: false
       t.string :email
 
       t.timestamps
